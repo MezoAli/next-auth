90bf76b47fb78537ed28a204a78fd79d5d59c8f7
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-01-14 14:34:56 +0200

    adding next-auth bvryptjs and api routes for creating user and storing it in mongodb database

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,38 +1,59 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef } from "react";
+import classes from "./auth-form.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+	const [isLogin, setIsLogin] = useState(true);
+	const emailRef = useRef();
+	const passwordRef = useRef();
 
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
-  }
+	function switchAuthModeHandler() {
+		setIsLogin((prevState) => !prevState);
+	}
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const enteredEmail = emailRef.current.value;
+		const enteredPassword = passwordRef.current.value;
+
+		fetch("/api/auth/signup", {
+			method: "POST",
+			body: JSON.stringify({
+				email: enteredEmail,
+				password: enteredPassword,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? "Login" : "Sign Up"}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor="email">Your Email</label>
+					<input type="email" id="email" ref={emailRef} required />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="password">Your Password</label>
+					<input type="password" id="password" ref={passwordRef} required />
+				</div>
+				<div className={classes.actions}>
+					<button>{isLogin ? "Login" : "Create Account"}</button>
+					<button
+						type="button"
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+						{isLogin ? "Create new account" : "Login with existing account"}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 }
 
 export default AuthForm;
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,45 @@
+import { MongoClient } from "mongodb";
+import { hash } from "bcryptjs";
+
+async function handler(req, res) {
+	let client;
+	const { email, password } = req.body;
+	if (
+		!email ||
+		!email.includes("@") ||
+		!password ||
+		password.trim().length < 7
+	) {
+		res.status(422).json({ message: "invalid inputs" });
+	}
+	try {
+		client = await MongoClient.connect(
+			"mongodb+srv://moutazali:1234567890@cluster0.vebfjx8.mongodb.net/users?retryWrites=true&w=majority"
+		);
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: error.message || "failed to connect to databse" });
+		return;
+	}
+
+	try {
+		const db = client.db();
+
+		const hashedPassword = await hash(password, 12);
+
+		await db.collection("authusers").insertOne({
+			email,
+			password: hashedPassword,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message || "failed to insert data" });
+		return;
+	}
+
+	res.status(201).json({ message: "created User" });
+
+	client.close();
+}
+
+export default handler;
